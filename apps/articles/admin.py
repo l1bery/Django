491438f8e2d2e1d0a899adf9cc240c79cec95c60
l1bery <emir555naz@gmail.com>
2025-07-11 17:33:52 +0600

    dada

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -6,12 +6,19 @@ from apps.articles.models import Genre,Game,Review
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'release_date', 'developer', 'publisher', 'age_rating')
-    search_fields = ('title', 'developer', 'publisher')
-    list_filter = ('release_date', 'age_rating', 'developer', 'publisher', 'genres')
+    list_display = ('id', 'title', 'release_date', 'developer', 'age_rating')
+    search_fields = ('title',)
+    list_filter = ('release_date', 'age_rating', 'developer', 'genres')
     ordering = ('title',)
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ('genres',)  # удобное отображение жанров
 
-    def game_count(self, obj):
-        return obj.game_set.count()  # Показывает кол-во игр в жанре
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'game', 'rating', 'created_at')
+    search_fields = ('user_name', 'game__title')
+    list_filter = ('rating', 'created_at', 'game')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
